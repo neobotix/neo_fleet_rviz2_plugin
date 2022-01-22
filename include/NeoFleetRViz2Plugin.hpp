2f14bf66507be6db423eb839aeb2e6e17a0f38a5
pradheep <padhupradheep@gmail.com>
2022-01-22 19:27:02 +0100

    improving linting

--- include/NeoFleetRViz2Plugin.hpp
+++ include/NeoFleetRViz2Plugin.hpp
@@ -1,25 +1,59 @@
-#ifndef INCLUDE_NEOFLEETRVIZ2PLUGIN_HPP_
-#define INCLUDE_NEOFLEETRVIZ2PLUGIN_HPP_
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2022, Neobotix GmbH
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Neobotix nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+#ifndef NEOFLEETRVIZ2PLUGIN_HPP_
+#define NEOFLEETRVIZ2PLUGIN_HPP_
 
 #include "rviz_common/display_context.hpp"
 #include "rclcpp/rclcpp.hpp"
-#include <rviz_common/panel.hpp>
-#include <rviz_common/config.hpp>
+#include "rviz_common/panel.hpp"
+#include "rviz_common/config.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "rviz_default_plugins/tools/goal_pose/goal_tool.hpp"
-#include <geometry_msgs/msg/pose_stamped.hpp>
-#include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
 #include "nav2_msgs/action/navigate_to_pose.hpp"
-#include <nav_msgs/msg/odometry.hpp>
-#include <QPushButton>
-#include <QThread>
-#include <QVBoxLayout>
-#include <QStringList>
-#include <QComboBox>
-#include <QLabel>
-#include <vector>
-#include <memory>
-#include <string>
+#include "nav_msgs/msg/odometry.hpp"
+#include "QPushButton"
+#include "QThread"
+#include "QVBoxLayout"
+#include "QStringList"
+#include "QComboBox"
+#include "QLabel"
+#include "vector"
+#include "memory"
+#include "string"
 
 
 class QLineEdit;
@@ -165,4 +199,4 @@ protected:
 };
 
 }  // namespace neo_fleet
-#endif  // MISSION_PANEL_H
+#endif  // NEOFLEETRVIZ2PLUGIN_HPP_
--- src/NeoFleetRViz2Plugin.cpp
+++ src/NeoFleetRViz2Plugin.cpp
@@ -1,103 +1,138 @@
-/*
- * neo_localization_node.cpp
+/*********************************************************************
+ * Software License Agreement (BSD License)
  *
- *  Created on: Sep 23, 2021
- *      Author: Pradheep Padmanabhan
- */
+ *  Copyright (c) 2022, Neobotix GmbH
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Neobotix nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
 #include "../include/NeoFleetRViz2Plugin.hpp"
 #include <geometry_msgs/msg/pose_with_covariance_stamped.h>
 
-#include <QPainter>
-#include <QLineEdit>
-#include <QVBoxLayout>
-#include <QHBoxLayout>
-#include <QLabel>
-#include <QTimer>
-#include <QComboBox>
+#include "string"
+#include "map"
+#include "vector"
+
+#include "QPainter"
+#include "QLineEdit"
+#include "QVBoxLayout"
+#include "QHBoxLayout"
+#include "QLabel"
+#include "QTimer"
+#include "QComboBox"
 
 namespace neo_fleet
 {
 
 // --- CONSTRUCTOR ---
-Worker::Worker() {
-    // you could copy data from constructor arguments to internal variables here.
+Worker::Worker()
+{
 }
- 
+
 // --- DECONSTRUCTOR ---
-Worker::~Worker() {
-    // free resources
+Worker::~Worker()
+{
 }
- 
+
 // --- PROCESS ---
 // Start processing data.
-void Worker::process() {
-    // allocate resources using new here
-    std::map<std::string, std::vector<std::string> > get_topic = node->get_topic_names_and_types(); 
-    std::string robots = "";
-    std::string tmp = "";
+void Worker::process()
+{
+  std::map<std::string, std::vector<std::string>> get_topic = node->get_topic_names_and_types();
+  std::string robots = "";
+  std::string tmp = "";
 
-    // Store the robots for the drop down list
-    for (auto it = get_topic.begin(); it != get_topic.end(); it++) {
-      int dslash = 0;
-      for (long unsigned int i = 0; i < (it->first).size(); i++) {
-        if (it->first[i] != '/') {
-          robots += it->first[i];  
-        }
-        if (it->first[i] == '/' && dslash != 2) {
-            dslash++;
-            if(dslash == 2 and tmp != robots) {
-              robot_namespaces.push_back(robots);
-              tmp = robots;
-            }
+  // Store the robots for the drop down list
+  for (auto it = get_topic.begin(); it != get_topic.end(); it++) {
+    int dslash = 0;
+    for (long unsigned int i = 0; i < (it->first).size(); i++) {
+      if (it->first[i] != '/') {
+        robots += it->first[i];
+      }
+      if (it->first[i] == '/' && dslash != 2) {
+        dslash++;
+        if (dslash == 2 && tmp != robots) {
+          robot_namespaces.push_back(robots);
+          tmp = robots;
         }
       }
-      robots = "";
     }
+    robots = "";
+  }
 
-    m_initial_pose = node->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>("/initialpose", 1, std::bind(&Worker::pose_callback, this, std::placeholders::_1));
-    m_goal_pose = node->create_subscription<geometry_msgs::msg::PoseStamped>("/goal_pose", 1, std::bind(&Worker::goal_callback, this, std::placeholders::_1));
+  m_initial_pose = node->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(
+    "/initialpose", 1, std::bind(&Worker::pose_callback, this, std::placeholders::_1));
+  m_goal_pose = node->create_subscription<geometry_msgs::msg::PoseStamped>(
+    "/goal_pose", 1, std::bind(&Worker::goal_callback, this, std::placeholders::_1));
 
-    rclcpp::Rate loop_rate(10);
-    while(rclcpp::ok()) {
-      loop_rate.sleep();   
-      rclcpp::spin_some(node);
-      emit finished();
-    }
+  rclcpp::Rate loop_rate(10);
+  while (rclcpp::ok()) {
+    loop_rate.sleep();
+    rclcpp::spin_some(node);
+    emit finished();
+  }
 }
 
-NeoFleetRViz2Plugin::NeoFleetRViz2Plugin( QWidget* parent )
-  : rviz_common::Panel( parent )
-
+NeoFleetRViz2Plugin::NeoFleetRViz2Plugin(QWidget * parent)
+: rviz_common::Panel(parent)
 {
-  QHBoxLayout* topic_layout = new QHBoxLayout;
+  QHBoxLayout * topic_layout = new QHBoxLayout;
   output_status_editor_ = new QLineEdit;
-  QTimer* output_timer = new QTimer( this );
+  QTimer * output_timer = new QTimer(this);
 
-  QPushButton* m_button1 = new QPushButton("RViz", this);
+  QPushButton * m_button1 = new QPushButton("RViz", this);
 
   combo = new QComboBox(this);
   robot_tmp = worker->Robot1;
 
-  topic_layout->addWidget( new QLabel( "Select the target robot:" ));
-  topic_layout->addWidget( combo );
-  topic_layout->addWidget( m_button1 );
+  topic_layout->addWidget(new QLabel("Select the target robot:"));
+  topic_layout->addWidget(combo);
+  topic_layout->addWidget(m_button1);
 
   // Lay out the topic field above the control widget.
-  connect( combo, QOverload<int>::of(&QComboBox::activated), this, &NeoFleetRViz2Plugin::setRobotName );
+  connect(
+    combo, QOverload<int>::of(&QComboBox::activated), this,
+    &NeoFleetRViz2Plugin::setRobotName);
   connect(m_button1, &QPushButton::released, this, &NeoFleetRViz2Plugin::handleButton1);
   // connect( output_timer, SIGNAL( timeout() ), this, SLOT( setRobotName(std::string) ));
 
   // Start the timer.
-  output_timer->start( 100 );
+  output_timer->start(100);
 
-  QVBoxLayout* layout = new QVBoxLayout;
+  QVBoxLayout * layout = new QVBoxLayout;
 
-  layout->addLayout( topic_layout );
-  layout1->addWidget( X_loc_value );
+  layout->addLayout(topic_layout);
+  layout1->addWidget(X_loc_value);
   layout1->addWidget(selected_robot);
-  layout->addLayout( layout1 );
+  layout->addLayout(layout1);
 
-  setLayout( layout );
+  setLayout(layout);
 
   worker->moveToThread(thread);
   connect(worker, SIGNAL(error(QString)), this, SLOT(errorString(QString)));
@@ -107,121 +142,123 @@ NeoFleetRViz2Plugin::NeoFleetRViz2Plugin( QWidget* parent )
   connect(worker, SIGNAL(finished()), worker, SLOT(deleteLater()));
   connect(thread, SIGNAL(finished()), thread, SLOT(deleteLater()));
   thread->start();
-
 }
 
 NeoFleetRViz2Plugin::~NeoFleetRViz2Plugin()
-  {
+{
+}
 
-  }
-
-void Worker::pose_callback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr pose ) {
+void Worker::pose_callback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr pose)
+{
   m_pose = pose;
 }
 
-void Worker::goal_callback(const geometry_msgs::msg::PoseStamped::SharedPtr pose ) {
+void Worker::goal_callback(const geometry_msgs::msg::PoseStamped::SharedPtr pose)
+{
   m_goal = pose;
 }
 
-void NeoFleetRViz2Plugin::setRobotName() {
-   robot_name = combo->currentText().toStdString();
-   if( robot_name == "mpo_7001") 
-   {
+void NeoFleetRViz2Plugin::setRobotName()
+{
+  robot_name = combo->currentText().toStdString();
+  if (robot_name == "mpo_7001") {
     robot_tmp = worker->Robot2;
-   } else {
+  } else {
     robot_tmp = worker->Robot1;
-   }
-
+  }
 }
 
-void NeoFleetRViz2Plugin::update() {
-
-  if(!process_combo) {
-    for (long unsigned int i = 0; i < worker->robot_namespaces.size(); i++)
-    {
-        robot_list.push_back(QString::fromStdString(worker->robot_namespaces[i]));
+void NeoFleetRViz2Plugin::update()
+{
+  if (!process_combo) {
+    for (long unsigned int i = 0; i < worker->robot_namespaces.size(); i++) {
+      robot_list.push_back(QString::fromStdString(worker->robot_namespaces[i]));
     }
     combo->addItems(robot_list);
     process_combo = true;
   }
 
-  if(!robot_tmp->m_robotLocalization) {
-    if(!worker->m_pose)
-      {
-        X_loc_value->setText("X: " + QString::number(0) + ", Y: " + QString::number(0) + ", Theta: " + QString::number(0));
-        selected_robot->setText("Selected Robot: " + QString::fromStdString(robot_tmp->robot_name));
-      }
-    else {
-        geometry_msgs::msg::PoseWithCovarianceStamped pub_pose;
-        selected_robot->setText("Selected Robot: " + QString::fromStdString(robot_tmp->robot_name));
-        X_loc_value->setText("X: " + QString::number(worker->m_pose->pose.pose.position.x) 
-          + ", Y: " + QString::number(worker->m_pose->pose.pose.position.y)
-          + ", Theta: " + QString::number(worker->m_pose->pose.pose.orientation.z));
-        pub_pose = *worker->m_pose;
-        robot_tmp->m_pub_loc_pose->publish(pub_pose);
-        m_localization_done = true;
-        robot_tmp->m_robotLocalization = true;
-        worker->m_pose = NULL;
-     }
+  if (!robot_tmp->m_robotLocalization) {
+    if (!worker->m_pose) {
+      X_loc_value->setText(
+        "X: " + QString::number(0) + ", Y: " + QString::number(
+          0) + ", Theta: " + QString::number(0));
+      selected_robot->setText("Selected Robot: " + QString::fromStdString(robot_tmp->robot_name));
+    } else {
+      geometry_msgs::msg::PoseWithCovarianceStamped pub_pose;
+      selected_robot->setText("Selected Robot: " + QString::fromStdString(robot_tmp->robot_name));
+      X_loc_value->setText(
+        "X: " + QString::number(worker->m_pose->pose.pose.position.x) +
+        ", Y: " + QString::number(worker->m_pose->pose.pose.position.y) +
+        ", Theta: " + QString::number(worker->m_pose->pose.pose.orientation.z));
+      pub_pose = *worker->m_pose;
+      robot_tmp->m_pub_loc_pose->publish(pub_pose);
+      m_localization_done = true;
+      robot_tmp->m_robotLocalization = true;
+      worker->m_pose = NULL;
+    }
   }
 
   if (robot_tmp->m_robotLocalization) {
     geometry_msgs::msg::PoseStamped pub_goal_pose;
     selected_robot->setText("Selected Robot: " + QString::fromStdString(robot_tmp->robot_name));
-    X_loc_value->setText("X: " + QString::number(robot_tmp->m_pose.pose.position.x) 
-        + ", Y: " + QString::number(robot_tmp->m_pose.pose.position.y)
-        + ", Theta: " + QString::number(robot_tmp->m_pose.pose.orientation.z));
+    X_loc_value->setText(
+      "X: " + QString::number(robot_tmp->m_pose.pose.position.x) +
+      ", Y: " + QString::number(robot_tmp->m_pose.pose.position.y) +
+      ", Theta: " + QString::number(robot_tmp->m_pose.pose.orientation.z));
 
-    if(worker->m_goal and robot_tmp->m_goalSent == false) {
+    if (worker->m_goal && robot_tmp->m_goalSent == false) {
       pub_goal_pose = *worker->m_goal;
       auto check_action_server_ready =
-      robot_tmp->navigation_action_client_->wait_for_action_server(std::chrono::seconds(5));
+        robot_tmp->navigation_action_client_->wait_for_action_server(std::chrono::seconds(5));
       if (!check_action_server_ready) {
-      RCLCPP_ERROR(
-        worker->node->get_logger(),
-        "navigate_to_pose action server is not available."
+        RCLCPP_ERROR(
+          worker->node->get_logger(),
+          "navigate_to_pose action server is not available."
         );
-      return;
+        return;
       }
-  
+
       robot_tmp->navigation_goal_.pose = pub_goal_pose;
 
       auto send_goal_options =
-      rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SendGoalOptions();
+        rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SendGoalOptions();
       send_goal_options.result_callback = [this](auto) {
-        robot_tmp->m_goalSent = false;
-      };
+          robot_tmp->m_goalSent = false;
+        };
 
       auto future_goal_handle =
-       robot_tmp->navigation_action_client_->async_send_goal(robot_tmp->navigation_goal_, send_goal_options);
+        robot_tmp->navigation_action_client_->async_send_goal(
+        robot_tmp->navigation_goal_,
+        send_goal_options);
       robot_tmp->m_goalSent = true;
       worker->m_goal = NULL;
-     }
+    }
   }
 }
 
 void NeoFleetRViz2Plugin::handleButton1()
 {
-  std::string command = "ros2 launch neo_simulation2 rviz_launch.py use_namespace:=true namespace:=";
+  std::string command =
+    "ros2 launch neo_simulation2 rviz_launch.py use_namespace:=true namespace:=";
   command.append(robot_tmp->robot_name);
   command.append("&");
 
-  system( command.c_str() );
+  system(command.c_str() );
 }
 
-void NeoFleetRViz2Plugin::save( rviz_common::Config config ) const
+void NeoFleetRViz2Plugin::save(rviz_common::Config config) const
 {
-  rviz_common::Panel::save( config);
+  rviz_common::Panel::save(config);
 }
 
 // Load all configuration data for this panel from the given Config object.
-void NeoFleetRViz2Plugin::load( const rviz_common::Config& config )
+void NeoFleetRViz2Plugin::load(const rviz_common::Config & config)
 {
-  rviz_common::Panel::load( config );
+  rviz_common::Panel::load(config);
 }
 
-} // namespace neo_fleet
+}  // namespace neo_fleet
 
-
-#include <pluginlib/class_list_macros.hpp>
+#include "pluginlib/class_list_macros.hpp"
 PLUGINLIB_EXPORT_CLASS(neo_fleet::NeoFleetRViz2Plugin, rviz_common::Panel)
